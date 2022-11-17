6fa1bc8e54b01a98925cf950b27e251d344d2290
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-11-17 15:46:35 -0300

    Versão: 1.0

---
+++ app/src/main/java/com/pablohbm/trabalho_final/Tela_Principal.java
@@ -0,0 +1,14 @@
+package com.pablohbm.trabalho_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
